56d9ac3db19b7a42ecd4f6564103b236440653b1
alisherzhorakhanov <59164659+alisherzhorakhanov@users.noreply.github.com>
2020-12-21 21:31:20 +0600

    Create footer.php

---
+++ blocks/footer.php
@@ -0,0 +1,60 @@
+<div class="footer">
+	<div class="foot">
+		<div class="useful_links">
+			
+			<span id="useful_links_logo">Useful links</span>
+			
+			<div class="links">
+				
+				<ul>
+					<li><a href="login.php">Login</a></li>
+					<li><a href="post_book.php">Post Book</a></li>
+					<li><a href="about.php">About us</a></li>
+					<li><a href="filter.php">Filter</a></li>
+					<li><a href="favorites.php">Favorites</a></li>
+				</ul>
+			</div>
+
+		</div>
+
+		<div class="contacts">
+		
+			<div class="get_in_touch">
+				<span id="contacts_logo">Contacts</span>
+				<span id="location"><i id="location_icon" class="fa fa-map-marker"></i> SDU,Kaskelen,Almaty,KZ</span>
+				<span id="phone"><i id="phone_icon" class="fa fa-mobile-phone"></i> Telephone: +77784232349 </span>
+				<span id="fax"><i id="faxIcon" class="fa fa-fax"></i> FAX: +1234 123</span>
+				<span id="email"><i id="emailIcon" class="fa fa-envelope"></i> Email: <a id="stu" href="#"> 180107024@stu.sdu.edu.kz</a></span>
+			</div>
+			
+		</div>
+		<div class="follow_us">
+			<div class="follow_logo">
+			<span id="follow_word">Follow Me</span>
+				
+				<div class="facebook">
+					<a href="https://vk.com/zhorakhanov1" target="_blank"><i id="facebook_icon" class="fa fa-facebook"></i></a>
+				</div>
+
+				<div class="google">
+					<a href="https://vk.com/zhorakhanov1" target="_blank"><i id="google_icon" class="fa fa-google-plus"></i></a>
+				</div>
+
+				<div class="vk">
+					<a href="https://vk.com/zhorakhanov1" target="_blank"><i id="vk_icon" class="fa fa-vk"></i></a>
+				</div>
+
+				<div class="instagram">
+					<a href="https://vk.com/zhorakhanov1" target="_blank"><i id="insta_icon" class="fa fa-instagram"></i></a>
+				</div>
+
+				<div class="github">
+					<a href="https://github.com/alisherzhorakhanov" target="_blank"><i id="git_icon" class="fa fa-github"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="rights">
+			<p>All rights reserved &copy; <?=date('Y')?></p>
+		</div>
+	</div>
